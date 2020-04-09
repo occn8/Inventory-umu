2fd323c55d58d29563048d8e1d3301d21f345bc1
occn8 <ochenwayward@gmail.com>
2020-04-09 14:49:33 +0300

    dep rm

--- lib/pages/barcode.dart
+++ lib/pages/barcode.dart
@@ -1,106 +1,55 @@
+// import 'dart:async';
+
+// import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
+// import 'package:flutter/services.dart';
 
-import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+class BarCode extends StatefulWidget {
+  @override
+  _BarCodeState createState() => _BarCodeState();
+}
 
-class BarCode extends StatelessWidget {
+class _BarCodeState extends State<BarCode> {
+  String barcode = "";
+
+  // @override
+  // initState() {
+  //   super.initState();
+  // }
+
+  // Future scan() async {
+  //   try {
+  //     String barcode = await BarcodeScanner.scan();
+  //     setState(() => this.barcode = barcode);
+  //   } on PlatformException catch (e) {
+  //     if (e.code == BarcodeScanner.CameraAccessDenied) {
+  //       setState(() {
+  //         this.barcode = 'Camera permission not granted!';
+  //       });
+  //     } else {
+  //       setState(() => this.barcode = 'Unknown error: $e');
+  //     }
+  //   } on FormatException {
+  //     setState(() => this.barcode =
+  //         'null ("back"-button pressed before scanning. Result)');
+  //   } catch (e) {
+  //     setState(() => this.barcode = 'Unknown error: $e');
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Text('bar code'),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // scan();
+        },
+        label: Text("Scan"),
+        icon: Icon(Icons.camera_alt),
+      ),
     );
-  }
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _scanBarcode = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  startBarcodeScanStream() async {
-    FlutterBarcodeScanner.getBarcodeStreamReceiver(
-            "#ff6666", "Cancel", true, ScanMode.BARCODE)
-        .listen((barcode) => print(barcode));
-  }
-
-  Future<void> scanQR() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          "#ff6666", "Cancel", true, ScanMode.QR);
-      print(barcodeScanRes);
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _scanBarcode = barcodeScanRes;
-    });
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> scanBarcodeNormal() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          "#ff6666", "Cancel", true, ScanMode.BARCODE);
-      print(barcodeScanRes);
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _scanBarcode = barcodeScanRes;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Barcode scan')),
-            body: Builder(builder: (BuildContext context) {
-              return Container(
-                  alignment: Alignment.center,
-                  child: Flex(
-                      direction: Axis.vertical,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        RaisedButton(
-                            onPressed: () => scanBarcodeNormal(),
-                            child: Text("Start barcode scan")),
-                        RaisedButton(
-                            onPressed: () => scanQR(),
-                            child: Text("Start QR scan")),
-                        RaisedButton(
-                            onPressed: () => startBarcodeScanStream(),
-                            child: Text("Start barcode scan stream")),
-                        Text('Scan result : $_scanBarcode\n',
-                            style: TextStyle(fontSize: 20))
-                      ]));
-            })));
   }
 }
