62580b2f7fc913f48f62042004963081b7c229d8
occn8 <ochenwayward@gmail.com>
2020-04-11 22:00:30 +0300

    change.

--- lib/pages/userview/userhome.dart
+++ lib/pages/userview/userhome.dart
@@ -2,11 +2,6 @@ import 'package:curve4/widgets/buttons.dart';
 import 'package:curve4/widgets/mydrawer.dart';
 import 'package:flutter/material.dart';
 
-// import 'Users.dart';
-// import 'admins.dart';
-// import 'barcode.dart';
-// import 'invmgt.dart';
-// import 'report.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
